34587440b57c319bd94481c757bdb35f804d66f8
Darkknight21YT <69129533+Darkknight21YT@users.noreply.github.com>
2021-03-12 23:14:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const Discordlogs = require("discord-logs");
+
 const TempChannels = require("discord-temp-channels");
 const tempChannels = new TempChannels(client);
 
 const config = require("./config.json");
-//const help = new Discord.MessageEmbed()
-//  .setColor('#0099ff')
-//  .setTitle('Help Command')
-//  .setDescription('This bot is in BETA. Valorant Manager controls the private VC function. This bot will have more functions soon.')
-//  .setTimestamp()
-//  .setFooter('This bot was coded by Darkknight#3011');
+
+var WelcomeChannel = 819754154796646430
+var Action = 820062322542379019
+
+client.on("guildBannerAdd", (guild, bannerURL) => {
+  let actionchannel = client.channels.cache.get(Action)
+  actionchannel.send(guild.name+" has a banner now!")
+});
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
       client.user.setActivity('Valorant Commuity', { type: "WATCHING"})
-  });
+});
 
 tempChannels.registerChannel("819685336598577172", {
     childCategory: "819383472644423691",
