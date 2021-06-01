5f294c8902e2925a738c383babfbc8f34252fc36
Darkknight21YT <69129533+Darkknight21YT@users.noreply.github.com>
2021-06-01 19:17:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ const config = require("./config.json");
 const color = require("./color.json")
 const prefix = config.prefix;
 
-const TempChannels = require("discord-temp-channels");
-const tempChannels = new TempChannels(client);
-
-tempChannels.registerChannel("819685336598577172", {
-    childCategory: "819383472644423691",
-    childAutoDeleteIfEmpty: true,
-    childAutoDeleteIfOwnerLeaves: true,
-    childMaxUsers: 5,
-    childBitrate: 64000,
-    childFormat: (member, count) => `#${count} | ${member.user.username}'s lounge`
-});
-
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
       client.user.setActivity('Valorant Community', { type: "WATCHING"})
