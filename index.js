df41b5bf879a84a86ce3a6492c52a4d581bde625
Darkknight21YT <69129533+Darkknight21YT@users.noreply.github.com>
2021-05-31 20:43:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,4 +22,44 @@ tempChannels.registerChannel("819685336598577172", {
     childFormat: (member, count) => `#${count} | ${member.user.username}'s VC`
 });
 
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+ 
+/* LOAD COMMAND FILES */
+fs.readdir('./commands/', (err, files) => {
+
+  if (err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+
+  if (jsfile.length <= 0) {
+    console.log("[WARNING]: Command folder is empty, No files loaded!")
+    return;
+  }
+
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`)
+    console.log(`Command ${f}`);
+    client.commands.set(props.help.name, props);
+    props.help.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name)
+    })
+  })
+
+  setTimeout(function() {
+  }, 1000)
+})
+    /* COMMANDS */
+
+  client.on("message", async message => {
+
+    let messageArray = message.content.split(" ");
+    let commandName = messageArray[0].toLowerCase();
+    let args = messageArray.slice(1);
+    let commandfile = client.commands.get(commandName.slice(prefix.length)) || client.commands.get(client.aliases.get(commandName.slice(prefix.length)));
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (commandfile) commandfile.run(client, message, args);
+  })
+
 client.login(config.token);
